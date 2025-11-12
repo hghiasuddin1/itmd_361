73299c427c6506cc89072d5d47a1616dafbcce46
Haaniya Ghiasuddin <123008313+hghiasuddin1@users.noreply.github.com>
2025-11-12 16:43:13 -0500

    Add init function to handle button click events

--- lab7/js/script.js
+++ lab7/js/script.js
@@ -1,1 +1,16 @@
+function init() {
+  const button = document.getElementById("entrybutton");
+  const input = document.getElementById("entryinput");
+  const output = document.getElementById("textoutput");
+
+  button.addEventListener("click", function () {
+
+    const message = input.value;
+    alert("Haaniya: " + message); 
+
+    output.textContent = message;
+  });
+}
+
+window.addEventListener("load", init);
 
